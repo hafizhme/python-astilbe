1af99690b35cad0ca482f298d20f1b6aa6bd68da
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-24 00:04:29 +0700

    Create if.py

---
+++ src/if.py
@@ -0,0 +1,6 @@
+your_name = input('What is your name? ')
+
+if your_name is 'Gollum':
+    print('MY PRECIOUS!!!')
+
+print('We wants it, we needs it. Must have the precious. They stole it from us. Sneaky little hobbitses. Wicked, tricksy, false!')
