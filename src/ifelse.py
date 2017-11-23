9f67b6437b79954fec889a471145effebb68b220
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-24 00:29:41 +0700

    Delete ifelse.py

--- src/ifelse.py
+++
@@ -1,7 +0,0 @@
-your_name = input('What is your name? ')
-
-if your_name == 'Gollum':
-    print('MY PRECIOUS!!!')
-else:
-    print('We wants it, we needs it. Must have the precious. They stole it from us. Sneaky little hobbitses. Wicked, tricksy, false!')
-    
