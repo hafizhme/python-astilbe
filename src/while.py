19366a1fa2c086d4ecb5f8312787b3eb3dd08522
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-24 02:09:38 +0700

    Update while.py

--- src/while.py
+++ src/while.py
@@ -1,11 +1,11 @@
 i = 1
 
 while i <= 10:
-	if i != 10:
-		print(i, end=', ')
-	else:
-		print(i)
-	
-	i += 1
+  if i != 10:
+    print(i, end=', ')
+  else:
+    print(i)
+  
+  i += 1
 	
 print('You got i =', i)
