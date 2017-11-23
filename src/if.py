7eefa407a812f5e22117bb912b91c73317e92ef0
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-24 00:28:41 +0700

    Delete if.py

--- src/if.py
+++
@@ -1,6 +0,0 @@
-your_name = input('What is your name? ')
-
-if your_name == 'Gollum':
-    print('MY PRECIOUS!!!')
-
-print('We wants it, we needs it. Must have the precious. They stole it from us. Sneaky little hobbitses. Wicked, tricksy, false!')
