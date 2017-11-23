060e7c879eccee714aff759eca17b497b6bdfdd5
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-24 02:06:28 +0700

    Update dowhile.py

--- src/dowhile.py
+++ src/dowhile.py
@@ -1,14 +1,14 @@
 i = 1
 
 while True:
-	if i != 10:
-		print(i, end=', ')
-	else:
-		print(i)
+  if i != 10:
+    print(i, end=', ')
+  else:
+    print(i)
+
+  i += 1
 	
-	i += 1
-	
-	if i == 11:
-		break
-		
+  if i == 11:
+    break
+
 print('You got i =', i)
