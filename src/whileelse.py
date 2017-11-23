177cb29d3f1165874e9321903bddb7907cbfb37e
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-24 02:10:10 +0700

    Update whileelse.py

--- src/whileelse.py
+++ src/whileelse.py
@@ -1,12 +1,12 @@
 i = 1
 
 while i <= 10:
-	if i != 10:
-		print(i, end=', ')
-	else:
-		print(i)
-	
-	i += 1
+  if i != 10:
+    print(i, end=', ')
+  else:
+    print(i)
+  
+  i += 1
 
 else:
-	print('Loop finished')
+  print('Loop finished')
