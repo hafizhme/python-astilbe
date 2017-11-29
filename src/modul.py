81aa563b96ca1c56efb85e5f0e8cf64b5f6a5e7d
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-29 13:50:57 +0700

    Create modul.py

---
+++ src/modul.py
@@ -0,0 +1,39 @@
+from fungsi import bintang_dua
+
+bintang_dua(a=1, b=2, c=3)
+
+
+
+
+
+
+
+import fungsi
+
+fungsi.bintang_dua(a=1, b=2, c=3)
+
+
+
+
+
+
+
+import fungsi as fn
+
+fn.bintang_dua(a=1, b=2, c=3)
+
+
+
+
+
+
+
+import math
+
+print(math.pow(2,18))
+
+
+
+
+
+
