0319c99fde22e8e0ac484757ecdbc59f62d18723
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-30 15:56:01 +0700

    Update line_1.py

--- src/line_1.py
+++ src/line_1.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
 data_a= {
-    'x': (1, 2, 3),
-    'y': (1, 2, 3),
+    'x': (1, 2, 3, 4, 5, 6, 7),
+    'y': (16, 8, 4, 2, 1, 2, 3),
 }
 
 plt.plot(data['x'], data['y'], label='data a')
