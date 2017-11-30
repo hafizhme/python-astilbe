f3cc696445737fd29115b58c84431d4588e4ea28
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-30 15:56:35 +0700

    Update line_2.py

--- src/line_2.py
+++ src/line_2.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
 data_a= {
-    'x': (1, 2, 3),
-    'y': (1, 2, 3),
+    'x': (1, 2, 3, 4, 5, 6, 7),
+    'y': (16, 8, 4, 2, 1, 2, 3),
 }
 
 data_b= {
-    'x': (1, 2, 3),
-    'y': (3, 4, 5),
+    'x': (1, 2, 3, 4, 5, 6, 7),
+    'y': (3, 2, 1, 2, 4, 8, 16),
 }
 
-plt.plot(data['x'], data['y'], label='data a')
-plt.plot(data['x'], data['y'], label='data b')
+plt.plot(data['x'], data['y'], label='data a', color='red')
+plt.plot(data['x'], data['y'], label='data b', color='blue')
 
 plt.show()
