687feabe7b99c69ec69d273c22e0f47e97794c93
Satria H R Harsono <hafizhme@users.noreply.github.com>
2017-11-30 19:07:23 +0700

    Update bar.py

--- src/bar.py
+++ src/bar.py
@@ -1,15 +1,5 @@
 import matplotlib.pyplot as plt
 
-data_a= {
-    'x': (1, 2, 3),
-    'y': (1, 2, 3),
-}
-
-data_b= {
-    'x': (1, 2, 3),
-    'y': (3, 4, 5),
-}
-
 plt.bar(data_a['x'], data_a['y'], label='data a', color='red')
 plt.bar(data_b['x'], data_b['y'], label='data b', color='blue')
 
